e9c7300c592a51eaa97ac3e3c840e7cbbafc3a1f
Herben <hrbdata17@gmail.com>
2025-10-16 20:41:08 -0300

    commit projeto

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,78 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String nome = "Herben Oliveira";
+        String tipoDeConta = "Corrente";
+        double saldo = 5000;
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String textoInicial = """
+                *****************************
+                
+                Dados do cliente
+                
+                Nome: %s
+                Tipo de conta: %s
+                Saldo Inicial: %.2f
+                
+                ******************************
+                """.formatted(nome, tipoDeConta, saldo);
+
+        System.out.println(textoInicial);
+
+        String menu = """
+                Operações
+                1 - Consultar saldo
+                2 - Receber valor
+                3 - Transferir valor
+                4 - Sair
+                """;
+
+        int operacao = 0;
+        while (operacao != 4) {
+
+            System.out.println(menu);
+            System.out.println("Digite a opção desejada:");
+            operacao = scanner.nextInt();
+
+            switch (operacao) {
+                case 1:
+                    System.out.println("Você selecionou a opção consulta de saldo");
+                    System.out.println("Seu saldo é: " + saldo);
+
+                    break;
+
+                case 2:
+                    System.out.println("Você selecionou a opção receber valor");
+                    System.out.println("Quanto você deseja receber?");
+                    double deposito = scanner.nextDouble();
+                    saldo += deposito;
+                    System.out.println("Deposito realizado! Seu novo saldo é R$" + saldo);
+                    break;
+
+                case 3:
+                    System.out.println("Você selecionou a opção transferir valor");
+                    System.out.println("Quanto você deseja transferir?");
+                    double transferencia = scanner.nextDouble();
+                    saldo -= transferencia;
+                    System.out.println("Você Retirou R$" + transferencia);
+                    System.out.println("Seu novo saldo é R$" + saldo);
+                    break;
+
+                case 4:
+                    System.out.println("Você selecionou a opção sair");
+                    System.out.println("Saindo da operação bancária");
+                    break;
+
+                default:
+                    System.out.println("Opção Inválida.");
+
+                    break;
+            }
+            System.out.println();
         }
+        System.out.println("Finalizando operações bancárias.");
+        scanner.close();
     }
 }
