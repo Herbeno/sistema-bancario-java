748993b08d96358647ce129ef156debe5d75d192
Herben <hrbdata17@gmail.com>
2025-10-16 21:12:43 -0300

    Atualização de validação de transferência

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,7 @@ public class Main {
         System.out.println(textoInicial);
 
         String menu = """
-                Operações
+                Menu de operações
                 1 - Consultar saldo
                 2 - Receber valor
                 3 - Transferir valor
@@ -55,9 +55,12 @@ public class Main {
                     System.out.println("Você selecionou a opção transferir valor");
                     System.out.println("Quanto você deseja transferir?");
                     double transferencia = scanner.nextDouble();
+                    if (transferencia <= saldo){
                     saldo -= transferencia;
                     System.out.println("Você Retirou R$" + transferencia);
-                    System.out.println("Seu novo saldo é R$" + saldo);
+                    System.out.println("Seu novo saldo é R$" + saldo);} else {
+                    System.out.println("Seu saldo é insuficiente para realizar esta transferencia.");
+                };
                     break;
 
                 case 4:
